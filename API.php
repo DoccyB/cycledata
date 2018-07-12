0e9c6fedaddda9333d9b2915eabe057330b2bcb7
DoccyB <tjbergquist@gmail.com>
2018-07-12 15:06:37 +0100

    Rewrote API URL

--- API.php
+++ API.php
@@ -8,7 +8,7 @@ class API
 	{
 		# retrieve data
 		$query = $this->bboxQuery ();
-		include '../database.php';
+		include 'database.php';
 		$database = new database ("cycletheft");
 
 		$data = $database->retrieveData ($query);
